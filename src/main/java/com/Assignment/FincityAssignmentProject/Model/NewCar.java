462e02167daf9b052fec7f8397a62a4b59703254
Vishal gupta <kumar.vg94@gmail.com>
2020-04-27 12:34:38 -0700

    Second Commit

--- src/main/java/com/Assignment/FincityAssignmentProject/Model/NewCar.java
+++ src/main/java/com/Assignment/FincityAssignmentProject/Model/NewCar.java
@@ -2,4 +2,32 @@ package com.Assignment.FincityAssignmentProject.Model;
 
 public class NewCar {
 
+	private int id;
+	private String name;
+	
+	private String Collage;
+	
+	
+	public String getCollage() {
+		return Collage;
+	}
+	public void setCollage(String collage) {
+		Collage = collage;
+	}
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	
+	
+	
+	
 }
